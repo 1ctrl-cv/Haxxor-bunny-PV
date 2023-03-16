de5453d0a9033e3365c4d5b8d28b1c05118ebdd9
1ctrl-cv <122760868+1ctrl-cv@users.noreply.github.com>
2023-03-16 20:20:52 +0800

    稍作修改

--- a/b/c/d/index.php
+++ a/b/c/d/index.php
@@ -1,9 +1,9 @@
 <html>
     <head>
-        #daimabugouchangzhushilaicou
+        <!--daimabugouchangzhushilaicou-->
         <title>4.7</title>
     </head>
     <body>
-    <?php echo ' <p>ternary notation1192998462</p>';?>
+    <?php echo '<p>ternary notation1192998462</p>';?>
     </body>
 </html>
